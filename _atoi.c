c9a74e414d0abf0b7bf2077fa419832bd88ec02c
Sarajeha <sarajehd@gmail.com>
2023-05-25 09:15:08 -0700

    _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -7,15 +7,15 @@
  * Return: 1 if interactive mode, 0 otherwise
  */
 int interactive(info_t *info)
-{       
-        return (isatty(STDIN_FILENO) && info->readfd <= 2);
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
- * is_delim - checks if charcter is a delimeter 
- * @c: the char to check 
- * @delim: the delimeter string 
- * Return: 1 if true, 0 if false 
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
  */
 int is_delim(char c, char *delim)
 {
@@ -26,23 +26,23 @@ int is_delim(char c, char *delim)
 }
 
 /**
- *_isalpha - checks for alphabetic character 
- *@c: The character to input 
- *Return: 1 if c is alphabetic, 0 otherwise 
+ * _isalpha - checks for alphabetic character
+ * @c: The character to input
+ * Return: 1 if c is alphabetic, 0 otherwise
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
-	else 
+	else
 		return (0);
 }
 
 /**
  * _atoi - converts a string to an integer
- * @s: the string to be converted 
- * Return: 0 if no numbers in string, converted numnber otherwise 
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
  */
 
 int _atoi(char *s)
@@ -58,7 +58,7 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			flag = 1;
-			result *+ 10;
+			result *= 10;
 			result += (s[i] - '0');
 		}
 		else if (flag == 1)
@@ -67,7 +67,9 @@ int _atoi(char *s)
 
 	if (sign == -1)
 		output = -result;
-	else 
+	else
 		output = result;
+
 	return (output);
+}
 
